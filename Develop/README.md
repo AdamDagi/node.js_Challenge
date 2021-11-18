18e304aa3d9a5bdd1fb32f91510f85e24bc65e05
AdamDagi <mugutdinovshiha02@gmail.com>
2021-11-18 15:17:26 -0500

    whrote question function

---
+++ Develop/README.md
@@ -0,0 +1,19 @@
+
+   # Professional README Generator Starter Code 
+      about node
+
+   #Instalation
+    yes
+
+   #Usage
+   yes
+
+   #Contributing
+   yes
+
+   #Tests
+   yes
+
+   #License
+   
+  
--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,55 @@
 // TODO: Include packages needed for this application
-
+const genMarkDown = require("./utils/generateMarkdown");
+const fs = require("fs");
+const inquirer = require("inquirer");
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        name: "Description",
+        message: "What is this Readme about?"
+    },
+    {
+        type: "input",
+        name: "Instalation",
+        message: "What is this Readme about?"
+    },
+    {
+        type: "input",
+        name: "Usage",
+        message: "What is this Readme about?"
+    },
+    {
+        type: "input",
+        name: "Contributing",
+        message: "What is this Readme about?"
+    },
+    {
+        type: "input",
+        name: "Tests",
+        message: "What is this Readme about?"
+    },
+    {
+        type: "list",
+        name: "License",
+        message: "question",
+        choices: ["", "", ""],
+        default: ""
+    }
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {};
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+        .prompt(questions)
+        .then((answers) => {
+            const text = genMarkDown(answers)
+            fs.writeFile("README.md", text, (e)=>{console.log(e)});
+        });
+};
 
 // Function call to initialize app
 init();
